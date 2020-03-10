45f936b7f2b0bc7c8316f24f589d89685bb51b79
nbaugh1 <nbaugh1@gmail.com>
2020-03-09 22:42:31 -0400

    build launch component with grapql query

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql')
 const schema = require('./schema')
+const cors = require('cors')
 
-// The root provides a resolver function for each API endpoint
+const app = express()
 
-const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(cors())
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
