6dd9ab018f7e98256d27ba54e3698a281611e1ad
nbaugh1 <nbaugh1@gmail.com>
2020-03-09 21:01:59 -0400

    create graphql schema and add npm scrpits to runa apollo server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const express = require('express')
+const schema = require('./schema')
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
